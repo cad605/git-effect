cb8d5bcb686cb627d31fe844b9df2ac5def60218
Christopher Donnelly <christopher.donnelly@compasspathways.com>
2026-03-19 20:50:37 -0400

    codecrafters submit [skip ci]

--- app/lib/parse-commit-body.ts
+++ app/lib/parse-commit-body.ts
@@ -4,28 +4,23 @@ import { CommitObject } from "../models/commit-object.ts";
 import { ObjectHash } from "../models/object-hash.ts";
 
 export const parseCommitBody = Effect.fn("parseCommitBody")(function* (body: Buffer) {
-  const [header, ...messageParts] = pipe(body.toString("utf8"), String.split("\n\n"));
+  const [content, ...messages] = pipe(body.toString("utf8"), String.split("\n\n"));
 
-  const message = pipe(messageParts, Array.join("\n\n"), String.trimEnd);
+  const message = pipe(messages, Array.join("\n\n"), String.trimEnd);
 
-  const [treeLine, ...rest] = String.split("\n")(header);
+  const [treeLine, ...rest] = String.split("\n")(content);
 
   const tree = yield* Schema.decodeUnknownEffect(ObjectHash)(treeLine.slice("tree ".length));
 
-  const [parentLines, remaining] = Array.span(rest, (line) => line.startsWith("parent "));
+  const [parentLines, [authorLine, committerLine]] = Array.span(rest, String.startsWith("parent "));
 
   const parents = yield* Effect.forEach(parentLines, (line) =>
     Schema.decodeUnknownEffect(ObjectHash)(String.slice("parent ".length)(line)),
   );
 
-  const author = pipe(remaining, Array.get(0), Option.getOrThrow, String.slice("author ".length));
+  const author = String.slice("author ".length)(authorLine);
 
-  const committer = pipe(
-    remaining,
-    Array.get(1),
-    Option.getOrThrow,
-    String.slice("committer ".length),
-  );
+  const committer = String.slice("committer ".length)(committerLine);
 
   return new CommitObject({ tree, parents, author, committer, message });
 });
